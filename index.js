a99c1883c60bf8e528e0a82d54d59c417058321f
Fiona Mellink <fimellink@gmail.com>
2023-12-17 07:17:47 +1100

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Fi",
+    streetAddress: "Ocean Road",
+}
+
+function updateEmployeeWithKeyAndValue(employee, employeeKey, employeeValue) {
+    const employeeUpdated  = {...employee};
+    employeeUpdated[employeeKey] = employeeValue;
+    return employeeUpdated
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, employeeKey, employeeValue) {
+    employee[employeeKey] = employeeValue;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const employeeUpdatedAgain = {...obj};
+    delete employeeUpdatedAgain[key];
+    return employeeUpdatedAgain;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
